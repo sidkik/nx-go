194758957a38001f993dbc56da1cdae7510f1d16
Mike Scheurwater <mike@codeowners.nl>
2021-11-16 09:47:43 +0100

    Fix Jest tsConfig error

--- e2e/nx-go-e2e/jest.config.js
+++ e2e/nx-go-e2e/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   preset: '../../jest.config.js',
   globals: {
     'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.spec.json',
+      tsconfig: '<rootDir>/tsconfig.spec.json',
     },
   },
   coverageDirectory: '../../coverage/e2e/nx-go-e2e',
