4ef71ce8a10174934bc8da1e64eb1e075a11489c
Mike Scheurwater <mike@codeowners.nl>
2021-11-17 11:28:34 +0100

    Fix lint-executor

--- packages/nx-go/src/executors/lint/executor.ts
+++ packages/nx-go/src/executors/lint/executor.ts
@@ -1,8 +1,18 @@
+import { ExecutorContext } from '@nrwl/devkit';
+import { join } from 'path';
+import { runGoCommand } from '../../utils/go-utils';
 import { LintExecutorSchema } from './schema';
 
-export default async function runExecutor(options: LintExecutorSchema) {
-  console.log('Executor ran for Lint', options);
+export default async function runExecutor(_options: LintExecutorSchema, context: ExecutorContext) {
+  if (!context.projectName) {
+    throw new Error('No projectName')
+  }
+  const appRoot = context.workspace.projects[context.projectName].root
+  const cwd = join(context.cwd, appRoot)
+
+  const sources = './...'
+
   return {
-    success: true,
+    success: runGoCommand('fmt', [sources], { cwd }),
   };
 }
