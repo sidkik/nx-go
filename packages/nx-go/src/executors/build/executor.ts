61ca2b388c660d05a39e76f79e4bbaca1f7c263f
Mike Scheurwater <mike@codeowners.nl>
2021-11-17 12:04:01 +0100

    Fix build-executor

--- packages/nx-go/src/executors/build/executor.ts
+++ packages/nx-go/src/executors/build/executor.ts
@@ -1,8 +1,11 @@
+import { runGoCommand } from '../../utils/go-utils';
 import { BuildExecutorSchema } from './schema';
 
 export default async function runExecutor(options: BuildExecutorSchema) {
-  console.log('Executor ran for Build', options);
+  const mainFile = `${options.main}`
+  const output = `-o ${options.outputPath}/main`
+
   return {
-    success: true,
+    success: runGoCommand('build', [output, mainFile]),
   };
 }
