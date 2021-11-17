994018763c4fb2a3a1c50d312be9704493859465
Mike Scheurwater <mike@codeowners.nl>
2021-11-17 11:41:18 +0100

    Fix serve-executor

--- packages/nx-go/src/executors/serve/executor.ts
+++ packages/nx-go/src/executors/serve/executor.ts
@@ -1,8 +1,19 @@
+import { ExecutorContext } from '@nrwl/devkit';
+import { join } from 'path';
+import { runGoCommand } from '../../utils/go-utils';
 import { ServeExecutorSchema } from './schema';
 
-export default async function runExecutor(options: ServeExecutorSchema) {
-  console.log('Executor ran for Serve', options);
+export default async function runExecutor(options: ServeExecutorSchema, context: ExecutorContext) {
+  if (!context.projectName) {
+    throw new Error('No projectName')
+  }
+  const project = context.workspace.projects[context.projectName];
+  const cwd = join(context.cwd, project.sourceRoot)
+
+  // We strip the project root from the main file
+  const mainFile = options.main?.replace(`${project.root}/`, '')
+
   return {
-    success: true,
+    success: runGoCommand('run', [mainFile], { cwd })
   };
 }
